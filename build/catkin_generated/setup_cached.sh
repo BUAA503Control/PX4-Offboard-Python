4aabe73459f562a0381149837f1d144b0960c18a
yrwang501 <yrwang501@hotmail.com>
2021-05-24 23:07:13 +0100

    All of File for caktin-py

---
+++ build/catkin_generated/setup_cached.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+# generated from catkin/python/catkin/environment_cache.py
+
+# based on a snapshot of the environment before and after calling the setup script
+# it emulates the modifications of the setup script without recurring computations
+
+# new environment variables
+
+# modified environment variables
+export CMAKE_PREFIX_PATH="/home/alex/catkin_ws_py/devel:$CMAKE_PREFIX_PATH"
+export PWD='/home/alex/catkin_ws_py/build'
+export ROS_PACKAGE_PATH="/home/alex/catkin_ws_py/src:$ROS_PACKAGE_PATH"
---
+++ build/offb_py/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/alex/catkin_ws_py/src/offb_py
+# Build directory: /home/alex/catkin_ws_py/build/offb_py
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
